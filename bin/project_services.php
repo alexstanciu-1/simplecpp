e8c92debbf340f32d37c097e80f071eab4414757
Alex Stanciu <ealexs@gmail.com>
2026-05-04 11:24:29 +0300

    Deduplicate lib-export project aggregate headers

--- bin/project_services.php
+++ bin/project_services.php
@@ -634,7 +634,7 @@ function execute_build(string $projectRoot, string $configPath): array
 		$projectContext['state']['project_root'] = $contextProjectRoot;
 		$projectContext['state']['updated_at'] = time();
 		save_s2s_state($projectContext['state_path'], $projectContext['state']);
-		write_text_file($projectContext['generated_dir'] . '/__project.hpp', render_project_export_header($contextProjectRoot, $projectContext['export_manifests'] ?? []));
+		write_text_file($projectContext['generated_dir'] . '/__project.hpp', render_project_export_header($projectContext['generated_dir'], $projectContext['export_manifests'] ?? []));
 	}
 	unset($projectContext);
 	write_text_file($buildDir . '/runtime_signature.txt', $runtimeBuildSignature . PHP_EOL);
@@ -832,43 +832,32 @@ function write_export_manifest_file(string $path, array $manifest): void
 }
 
 /** @param list<string> $exportManifestPaths */
-function render_project_export_header(string $projectRoot, array $exportManifestPaths): string
+function render_project_export_header(string $generatedDir, array $exportManifestPaths): string
 {
 	$lines = ['#pragma once', '', '#include <scpp/lang/php.hpp>'];
-	$seenBlocks = [];
+	$seenHeaders = [];
 	foreach (array_values(array_unique($exportManifestPaths)) as $manifestPath) {
-		$manifest = load_export_manifest($manifestPath);
-		$prologueIncludes = is_array($manifest['prologue_includes'] ?? null) ? $manifest['prologue_includes'] : [];
-		foreach ($prologueIncludes as $includePath) {
-			if (!is_string($includePath) || trim($includePath) === '') {
-				continue;
-			}
-			$includeLine = '#include "' . trim($includePath) . '"';
-			if (!in_array($includeLine, $lines, true)) {
-				$lines[] = $includeLine;
-			}
+		$headerPath = export_manifest_header_path($manifestPath);
+		if ($headerPath === null) {
+			continue;
 		}
-		foreach (is_array($manifest['namespaces'] ?? null) ? $manifest['namespaces'] : [] as $namespaceManifest) {
-			$headerLines = is_array($namespaceManifest['header_lines'] ?? null) ? $namespaceManifest['header_lines'] : [];
-			$blockKey = json_encode($headerLines, JSON_UNESCAPED_SLASHES);
-			if (!is_string($blockKey) || isset($seenBlocks[$blockKey])) {
-				continue;
-			}
-			$seenBlocks[$blockKey] = true;
-			if ($lines !== [] && end($lines) !== '') {
-				$lines[] = '';
-			}
-			foreach ($headerLines as $line) {
-				if (is_string($line)) {
-					$lines[] = $line;
-				}
-			}
+		$includeTarget = normalize_config_path(relative_path($generatedDir, $headerPath));
+		if ($includeTarget === '' || isset($seenHeaders[$includeTarget])) {
+			continue;
 		}
+		$seenHeaders[$includeTarget] = true;
+		$lines[] = '#include "' . $includeTarget . '"';
 	}
-	if ($lines === ['#pragma once', '', '#include <scpp/lang/php.hpp>']) {
-		$lines[] = '';
-	}
+	$lines[] = '';
 	return implode(PHP_EOL, $lines) . PHP_EOL;
+}
+
+function export_manifest_header_path(string $manifestPath): ?string
+{
+	if (!str_ends_with($manifestPath, '.exports.json')) {
+		return null;
+	}
+	return substr($manifestPath, 0, -strlen('.exports.json')) . '.hpp';
 }
 
 /** @return array<string,mixed> */
